f18b67bc67a35fd45fea32599f62ec6cffdb2677
Leo Maia <32986455+leomaia97@users.noreply.github.com>
2018-02-20 19:35:51 -0300

    Add files via upload

---
+++ Aula2_CallingFunction.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int saudacao(){
+        printf("Boa Noite");
+    }
+
+int main(){
+    saudacao();
+    return 0;
+}
+
+
+
+
+/*int saudacao(char);
+
+void main(){
+
+char c;
+
+c = saudacao()
+    printf("%s",&c);
+
+
+    }
+
+
+        //meu codigo acima
+        // DEPOIS TENTAR FAZER TESTE JEITO AQUI
+        //codigo da internet abaixo
+#include<stdio.h>
+#include<conio.h>
+
+int sum(int,int);
+
+void main()
+{
+int a,b,c;
+printf("\nEnter the two numbers : ");
+scanf("%d%d",&a,&b);
+
+c = sum(a,b);
+
+printf("\nAddition of two number is : %d",c);
+getch();
+}
+
+int sum (int num1,int num2)
+{
+int num3;
+num3 = num1 + num2 ;
+
+return(num3);
+}
+
+*/
